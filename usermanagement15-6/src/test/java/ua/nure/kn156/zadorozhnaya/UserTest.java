0db83a684814af6459c4dc463fc45cfd872de0ec
irynazadorozhnaya <iryna.zadorozhna@nure.ua>
2017-10-10 23:36:06 +0300

    Initial commit for usermanagement

---
+++ usermanagement15-6/src/test/java/ua/nure/kn156/zadorozhnaya/UserTest.java
@@ -0,0 +1,32 @@
+package ua.nure.kn156.zadorozhnaya;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import junit.framework.TestCase;
+
+public class UserTest extends TestCase {
+	
+	private User user;
+	private Date dateOfBirthd;
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		user = new User();
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(1998, calendar.FEBRUARY, 4);
+		dateOfBirthd = calendar.getTime();
+	}
+
+	public void testGetFullName() {
+		user.setFirstName("Irina");
+		user.setLastName("Zadorozhnaya");
+		assertEquals("Zadorozhnaya, Irina", user.getFullName());
+	}
+	
+	public void testGetAge() {
+		user.setDateOfBirthd(dateOfBirthd);
+		assertEquals(2017 - 1998, user.getAge());
+	}
+}
