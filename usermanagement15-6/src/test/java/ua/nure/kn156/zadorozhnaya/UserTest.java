b1c212b6c858e82f413616a0bd0484c8de0669e5
irynazadorozhnaya <iryna.zadorozhna@nure.ua>
2017-10-29 19:48:39 +0200

    provided .gitignore file, code style applied and constructors for User class

--- usermanagement15-6/src/test/java/ua/nure/kn156/zadorozhnaya/UserTest.java
+++ usermanagement15-6/src/test/java/ua/nure/kn156/zadorozhnaya/UserTest.java
@@ -6,27 +6,27 @@ import java.util.Date;
 import junit.framework.TestCase;
 
 public class UserTest extends TestCase {
-	
-	private User user;
-	private Date dateOfBirthd;
 
-	protected void setUp() throws Exception {
-		super.setUp();
-		user = new User();
-		
-		Calendar calendar = Calendar.getInstance();
-		calendar.set(1998, calendar.FEBRUARY, 4);
-		dateOfBirthd = calendar.getTime();
-	}
+    private User user;
+    private Date dateOfBirthd;
 
-	public void testGetFullName() {
-		user.setFirstName("Irina");
-		user.setLastName("Zadorozhnaya");
-		assertEquals("Zadorozhnaya, Irina", user.getFullName());
-	}
-	
-	public void testGetAge() {
-		user.setDateOfBirthd(dateOfBirthd);
-		assertEquals(2017 - 1998, user.getAge());
-	}
+    protected void setUp() throws Exception {
+        super.setUp();
+        user = new User();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(1998, calendar.FEBRUARY, 4);
+        dateOfBirthd = calendar.getTime();
+    }
+
+    public void testGetFullName() {
+        user.setFirstName("Irina");
+        user.setLastName("Zadorozhnaya");
+        assertEquals("Zadorozhnaya, Irina", user.getFullName());
+    }
+
+    public void testGetAge() {
+        user.setDateOfBirthd(dateOfBirthd);
+        assertEquals(2017 - 1998, user.getAge());
+    }
 }
